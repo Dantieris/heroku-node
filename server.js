fc9dc7fd2bee7aba164d9c72cbaa472268aa48f2
Dantiéris Castilhos Rabelini <dantieris@gmail.com>
2016-11-26 15:11:20 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 var http = require('http');
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
 http.createServer(function (req, res) {
